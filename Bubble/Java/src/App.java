73606791cda557cde1dc3cd531086e693ad1ccfd
Vinícius Rodrigues <viniciusrodrigues1108@gmail.com>
2024-11-23 18:44:54 -0300

    java

--- Bubble/Java/src/App.java
+++ Bubble/Java/src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) {
+        long startTime = System.nanoTime(); // Início da medição
         int[] data = {64, 34, 25, 12, 22, 11, 90};
         BubbleSort.sort(data);
 
@@ -9,5 +10,10 @@ public class App {
             System.out.print(num + " ");
         }
         System.out.print("]");
+
+        long endTime = System.nanoTime(); // Fim da medição
+        long duration = endTime - startTime; // Tempo total em nanossegundos
+
+        System.out.println("Tempo de execução: " + duration + " nanosegundos");
     }
 }
--- Quick/Java/src/App.java
+++ Quick/Java/src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) {
+        long startTime = System.nanoTime(); // Início da medição
         int[] data = {64, 34, 25, 12, 22, 11, 90};
 
         // Chama o QuickSort
@@ -9,5 +10,10 @@ public class App {
         for (int num : data) {
             System.out.print(num + " ");
         }
+        long endTime = System.nanoTime(); // Fim da medição
+        long duration = endTime - startTime; // Tempo total em nanossegundos
+
+        System.out.println("Tempo de execução: " + duration + " nanosegundos");
+
     }
 }
